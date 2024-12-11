05088b6c58e169a8021543b0ab0e40dfeb60695a
sialex-net <91857463+sialex-net@users.noreply.github.com>
2024-12-11 12:12:24 +0700

    swap vitePluginViteNodeMiniflare

--- workers/app.ts
+++ workers/app.ts
@@ -1,12 +1,16 @@
+import "../worker-configuration.d.ts"
 import { createRequestHandler } from "react-router";
 
 declare global {
-  interface CloudflareEnvironment {}
+  interface CloudflareEnvironment extends Env {}
 }
 
 declare module "react-router" {
   export interface AppLoadContext {
-    VALUE_FROM_CLOUDFLARE: string;
+    cloudflare: {
+      env: CloudflareEnvironment;
+      ctx: ExecutionContext;
+    };
   }
 }
 
@@ -17,9 +21,9 @@ const requestHandler = createRequestHandler(
 );
 
 export default {
-  fetch(request, env) {
+  fetch(request, env, ctx) {
     return requestHandler(request, {
-      VALUE_FROM_CLOUDFLARE: "Hello from Cloudflare",
+      cloudflare: { env, ctx },
     });
   },
 } satisfies ExportedHandler<CloudflareEnvironment>;
